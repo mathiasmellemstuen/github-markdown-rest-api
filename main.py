6016dd5c96f6cd83a4499940a32692d896b09b9f
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2024-01-09 16:29:01 +0100

    Adding changes

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def amount_of_visitors():
 
     content = get_amount_of_unique_visitors()
  
-    content = f'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100" height="20"><style>.letters { fill: #2E2E2E; } @media (prefers-color-scheme: dark) { .letters { fill: #ffffff; }}</style><g font-family="Verdana,DejaVu Sans,Geneva,sans-serif" font-size="11"><text class="letters" x="0" y="14">{content}</text></g></svg>'
+    content = f'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100" height="20"><style>.letters \{ fill: #2E2E2E; \} @media (prefers-color-scheme: dark) \{ .letters \{ fill: #ffffff; \}\}</style><g font-family="Verdana,DejaVu Sans,Geneva,sans-serif" font-size="11"><text class="letters" x="0" y="14">{content}</text></g></svg>'
     response = Response(content)
     response.content_type = "image/svg+xml"
     response.expires = 0
