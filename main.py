8577780c12bd88789c346e98efe4910b2e37b7f6
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2024-01-09 12:13:30 +0100

    Adding minor changes.

--- main.py
+++ main.py
@@ -57,4 +57,4 @@ def amount_of_visitors():
     return response 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8001, debug=True)
