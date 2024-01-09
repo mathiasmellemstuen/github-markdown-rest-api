9af14d1b0ef37b02a4e8aca834149b7123af1131
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2024-01-09 11:45:59 +0100

    Adding changes.

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 from flask import Flask, Response, request
 import sqlite3
 from datetime import datetime
+import re
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
+def is_ipv4(ip_string): 
+    regex_ipv4 = "(\d{1,3}\.){3}\d{1,3}"
+    return bool(re.search(regex_ipv4, ip_string))
+
+def is_ipv6(ip_string): 
+    regex_ipv6 = "((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9a-f]{1,4}:){7}([0-9a-f]{1,4}|:))|(([0-9a-f]{1,4}:){6}(:[0-9a-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9a-f]{1,4}:){5}(((:[0-9a-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9a-f]{1,4}:){4}(((:[0-9a-f]{1,4}){1,3})|((:[0-9a-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9a-f]{1,4}:){3}(((:[0-9a-f]{1,4}){1,4})|((:[0-9a-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9a-f]{1,4}:){2}(((:[0-9a-f]{1,4}){1,5})|((:[0-9a-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9a-f]{1,4}:){1}(((:[0-9a-f]{1,4}){1,6})|((:[0-9a-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9a-f]{1,4}){1,7})|((:[0-9a-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))"
+    return bool(re.search(regex_ipv6, ip_string))
+
 def add_visitor(address, time): 
+
+    # Checking the input address, if its not valid
+    if not is_ipv4(address) and not is_ipv6(address):
+        return
+
     database = sqlite3.connect("database.db")
 
     query = f"INSERT INTO visitors VALUES ('{address}','{time}')"
     database.execute(query)
     database.commit()
     database.close()
+
 
 def get_amount_of_unique_visitors():
     database = sqlite3.connect("database.db")
@@ -33,14 +48,10 @@ def amount_of_visitors():
 
     add_visitor(address, time)
 
-    # res = database.execute("SELECT * FROM visitors")
-    # print(res.fetchall())
     content = get_amount_of_unique_visitors()
     content = f'<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100" height="20"><g font-family="Verdana,DejaVu Sans,Geneva,sans-serif" font-size="11"><text x="0" y="14">{content}</text></g></svg>'
-    # content = Markup(content)
     response = Response(content)
     response.content_type = "image/svg+xml"
-    # response.cache_control = "no-cache, no-store, must-revalidate"
     response.expires = 0
 
     return response 
