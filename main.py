5bb1619f9e43bc567c1e9f2bfc7f060f856730bb
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2024-01-10 19:28:26 +0100

    Minor fix.

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def get_amount_of_visitors():
     return data[0][0]
 
 
-@app.route("/amount_of_visitors.svg")
+@app.route("/amount_of_unique_visitors.svg")
 def amount_of_unique_visitors():
 
     time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
